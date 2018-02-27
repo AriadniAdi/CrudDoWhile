df865d401026f2d3b0e2716a99c3c5cf5881f810
Ariadni Chioquetta <ariadniadi@gmail.com>
2018-02-27 03:51:33 -0300

    retirada de estilos css do arquivo app.vue a fim de organização

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <header>
-      <nav width="200px" heigth="100px"></nav>
-  </header>
     <router-view/>
   </div>
 </template>
@@ -13,32 +10,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 50px;
-}
+<style scoped>
 
-header {
-  width: 200px;
-}
-
-nav {
-  text-align: left;
-
-}
-
-container {
-  height: 300px;
-  display: -webkit-flex;
-  display:         flex;
-  -webkit-align-items: center;
-          align-items: center;
-  -webkit-justify-content: center;
-          justify-content: center;
-}
 </style>
